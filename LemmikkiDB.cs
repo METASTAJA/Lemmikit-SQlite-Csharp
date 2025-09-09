ec0cef3f8154800e6d2a1f0bd31e9f7d394a8fd5
Aatu <liimataisenaatu@gmail.com>
2025-09-09 12:52:29 +0300

    adding tables

--- LemmikkiDB.cs
+++ LemmikkiDB.cs
@@ -14,9 +14,16 @@ public class LemmikkiDB
         //avaa yhteyden tietokantaan
         connection.Open();
         //luodaan taulut, jos niitä ei vielä ole
-        var commandForTableCreation = connection.CreateCommand();
-        commandForTableCreation.CommandText = "CREATE TABLE IF NOT EXISTS Omistajat (id INTEGER PRIMARY KEY,nimi TEXT,puhelin INT)";
-        commandForTableCreation.ExecuteNonQuery(); //emmen odota vastausta tältä komennolta
+        //Omistajat taulu
+        var commandForOmistjat = connection.CreateCommand();
+        commandForOmistjat.CommandText = "CREATE TABLE IF NOT EXISTS Omistajat (id INTEGER PRIMARY KEY,nimi TEXT,puhelin INT)";
+        commandForOmistjat.ExecuteNonQuery(); //emmen odota vastausta tältä komennolta
+
+        //Lemmitikit taulu
+        var commandForLemmikit = connection.CreateCommand();
+        commandForLemmikit.CommandText = "CREATE TABLE IF NOT EXISTS Lemmikit (id INTEGER PRIMARY KEY,nimi TEXT,laji TEXT,omistajan_id INTEGER)";
+        commandForLemmikit.ExecuteNonQuery(); //emmen odota vastausta tältä komennolta
+
         connection.Close();
 
 
